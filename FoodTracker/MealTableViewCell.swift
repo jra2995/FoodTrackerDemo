9c438d4313a1aebbba017e37eee4f55ad24afee2
Jett Anderson <jettanderson@jetts-macbook-pro.local>
2017-07-01 01:05:45 -0500

    Create Data Model, Begin Work on TableView

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jett Anderson on 7/1/17.
+//
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
